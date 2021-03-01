6c8ece69df1e024d19aa40936859c21a66515bec
limheonjun <jumong40@naver.com>
2021-03-01 21:29:04 +0900

    remeber-me 설정

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,11 +1,13 @@
 package io.security.basicsecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
@@ -20,6 +22,9 @@ import java.io.IOException;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -74,5 +79,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 })
                 // 쿠키삭제
                 .deleteCookies("remember-me");
+
+        /* remember-me 설정 */
+        http
+                .rememberMe()
+                .rememberMeParameter("remember")            // 기본 파라미터명 변경(기본값 : remember-me)
+                .tokenValiditySeconds(3600)                 // 기본값은 14일
+                .userDetailsService(userDetailsService);
     }
 }
